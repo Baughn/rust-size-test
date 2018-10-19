163430a81d5d68aab4a8110f40f77e30503f5de0
Svein Ove Aas <sveina@gmail.com>
2018-10-19 20:24:44 +0100

    Add some profile options, gaining 1%

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ set -eu -o pipefail
 TARGET=x86_64-unknown-linux-musl
 
 rustup target add x86_64-unknown-linux-musl
-cargo build --release --target=x86_64-unknown-linux-musl
+cargo build --release --target $TARGET
 strip target/$TARGET/release/sztest
 upx target/$TARGET/release/sztest
 target/$TARGET/release/sztest
